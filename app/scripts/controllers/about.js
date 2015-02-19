63be19ef2c8f4c1b676346e105b725a5d3d56b06
Adam Smith <abs2g08@gmail.com>
2015-02-18 23:18:38 -0800

    First awsome commit of an awsome app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tubeview2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tubeview2App
+ */
+angular.module('tubeview2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tubeview2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tubeview2App
+ */
+angular.module('tubeview2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
