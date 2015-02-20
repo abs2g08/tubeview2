eb68fb3e4b1d0f0957a807226d7de6709e0ad977
Adam Smith <abs2g08@gmail.com>
2015-02-19 22:32:32 -0800

    Made some small changes to youtubeAPI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the tubeview2App
  */
 angular.module('tubeview2App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $alert, youtubeAPI) {
+
+    youtubeAPI.findVideo('test').then(function(data) {
+
+    }).catch(function(resp) {
+      $alert({
+        title: 'Error',
+        content: 'There was an error finding videos: '+resp.data.error.message,
+        placement: 'top',
+        duration: 3,
+        type: 'danger',
+        show: true
+      });
+    });
+
   });
