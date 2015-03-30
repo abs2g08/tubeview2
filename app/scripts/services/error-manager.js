3b055766d153867bb31c19c92a6d2e1d136eeaa4
abs20g8 <abs2g08@gmail.com>
2015-03-29 18:56:57 -0700

    about to remvove stuff

--- app/scripts/services/error-manager.js
+++ app/scripts/services/error-manager.js
@@ -10,9 +10,6 @@
 angular.module('tubeview2App')
   .factory('ErrorManager', function($rootScope, dialogs, $translate) {
 
-    $rootScope.lang = 'en-US';
-    $rootScope.language = 'English';
-
     var errorManager = {
       handleHttpError: function(event, resp) {
         debugger;
@@ -28,20 +25,4 @@ angular.module('tubeview2App')
     $rootScope.$on('httpError', errorManager.handleHttpError);
 
     return errorManager;
-  })
-      $translateProvider.translations('en-US',{
-              DIALOGS_ERROR: "Error",
-              DIALOGS_ERROR_MSG: "An unknown error has occurred.",
-              DIALOGS_CLOSE: "Close",
-              DIALOGS_PLEASE_WAIT: "Please Wait",
-              DIALOGS_PLEASE_WAIT_ELIPS: "Please Wait...",
-              DIALOGS_PLEASE_WAIT_MSG: "Waiting on operation to complete.",
-              DIALOGS_PERCENT_COMPLETE: "% Complete",
-              DIALOGS_NOTIFICATION: "Notification",
-              DIALOGS_NOTIFICATION_MSG: "Unknown application notification.",
-              DIALOGS_CONFIRMATION: "Confirmation",
-              DIALOGS_CONFIRMATION_MSG: "Confirmation required.",
-              DIALOGS_OK: "OK",
-              DIALOGS_YES: "Yes",
-              DIALOGS_NO: "No"
-          });
+  });
