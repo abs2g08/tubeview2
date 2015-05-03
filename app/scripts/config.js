b2d5f6c5ff486f2669be2beab3d3c936facfa755
abs20g8 <abs2g08@gmail.com>
2015-05-03 15:17:00 -0700

    fixed up config file

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -7,8 +7,8 @@ angular.module('tubeview2App')
   })
 
   .value('GoogleApp', {
-      apiKey: 'AIzaSyBv6-Q7ZSZkFOZ4lY1yI-BAx6EMiXJK0RU',
-      clientId: '16734044308-a5nrp2dk6qseg3ro66ar317m1cpt4c2m.apps.googleusercontent.com',
+      apiKey: '<api key>',
+      clientId: '<client key>',
       scopes: [
         'https://www.googleapis.com/auth/youtube',
       ]
