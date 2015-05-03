893e7bd5bf55473acc6595eb65e94121b536e091
abs20g8 <abs2g08@gmail.com>
2015-05-03 15:15:47 -0700

    fixed up some css issues

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -7,8 +7,8 @@ angular.module('tubeview2App')
   })
 
   .value('GoogleApp', {
-      apiKey: '<api key>',
-      clientId: '<client id>',
+      apiKey: 'AIzaSyBv6-Q7ZSZkFOZ4lY1yI-BAx6EMiXJK0RU',
+      clientId: '16734044308-a5nrp2dk6qseg3ro66ar317m1cpt4c2m.apps.googleusercontent.com',
       scopes: [
         'https://www.googleapis.com/auth/youtube',
       ]
